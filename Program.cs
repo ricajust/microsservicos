a6466e53f0e2c8f035ec61dedf0d3fdd9df99cb1
Ricajust <ricartoonstudio@gmail.com>
2025-03-19 20:28:58 -0300

    feat: registro do servico e repositorio para o ef usar as implementacoes

--- Program.cs
+++ Program.cs
@@ -1,13 +1,21 @@
 using Alunos.API.Data;
+using Alunos.API.Repositories;
+using Alunos.API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Adicionando serviços ao container
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer("Server=localhost,1435;Database=AlunosDb;User ID=sa;Password=Edug0@2025!;TrustServerCertificate=True"));
+builder.Services.AddControllers();
+
+// Registrando os serviços e repositórios
+builder.Services.AddScoped<IAlunoService, AlunoService>();
+builder.Services.AddScoped<IUsuarioService, UsuarioService>();
+builder.Services.AddScoped<IAlunoRepository, AlunoRepository>();
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
     
 var app = builder.Build();
 
